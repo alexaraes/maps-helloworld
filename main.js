0193d18401bb7e1f6fd488a4102ecc6511b6fe8f
Alexa Schreffler <alexa.schreffler@gmail.com>
2015-07-05 20:50:00 -0500

    hello world for google maps API

---
+++ main.js
@@ -0,0 +1,26 @@
+function initialize() {
+  var myLatlng = new google.maps.LatLng(30.252738,-97.723682);
+  var mapOptions = {
+    zoom: 10,
+    center: myLatlng
+  }
+  var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+  var contentString = '<div id="content">'+
+      '<div id="siteNotice">'+
+      '</div>'+
+      '<h1 id="firstHeading" class="firstHeading">Launderette</h1>'+
+      '</div>';
+
+  var infowindow = new google.maps.InfoWindow({
+      content: contentString
+  });
+
+  var marker = new google.maps.Marker({
+      position: myLatlng,
+      map: map,
+      title: 'Launderette'
+  });
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
